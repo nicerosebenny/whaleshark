1b83cbe1e9bcb913860d6596ce431abb5631ec17
BrintaThomas <117457186+BrintaThomas@users.noreply.github.com>
2022-11-18 18:24:05 +0530

    login

--- lib/register.dart
+++ lib/register.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -9,9 +8,11 @@ class Register extends StatefulWidget {
   State<Register> createState() => _RegisterState();
 }
 
+// ignore: camel_case_types
 enum user { female, male, other }
 
 class _RegisterState extends State<Register> {
+  // ignore: prefer_final_fields
   user? _gender = user.female;
   @override
   Widget build(BuildContext context) {
@@ -19,17 +20,17 @@ class _RegisterState extends State<Register> {
         backgroundColor: Colors.lightBlue.shade200,
         appBar: AppBar(
           backgroundColor: Colors.lightBlue,
-          title: Text("Registration"),
+          title: const Text("Registration"),
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 5),
+            const SizedBox(height: 5),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
               child: Column(
                 children: [
-                  TextField(
+                  const TextField(
                     style: TextStyle(color: Colors.black),
                     decoration: InputDecoration(
                       labelText: ("Name:"),
@@ -39,8 +40,8 @@ class _RegisterState extends State<Register> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 5),
-                  TextField(
+                  const SizedBox(height: 5),
+                  const TextField(
                     style: TextStyle(color: Colors.black),
                     decoration: InputDecoration(
                       labelText: ("Phone Number:"),
@@ -50,8 +51,8 @@ class _RegisterState extends State<Register> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 5),
-                  TextField(
+                  const SizedBox(height: 5),
+                  const TextField(
                     style: TextStyle(color: Colors.black),
                     decoration: InputDecoration(
                       labelText: ("E-mail id:"),
@@ -61,8 +62,8 @@ class _RegisterState extends State<Register> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 5),
-                  TextField(
+                  const SizedBox(height: 5),
+                  const TextField(
                     style: TextStyle(color: Colors.black),
                     decoration: InputDecoration(
                       labelText: ("Address:"),
@@ -72,8 +73,8 @@ class _RegisterState extends State<Register> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 5),
-                  TextField(
+                  const SizedBox(height: 5),
+                  const TextField(
                     style: TextStyle(color: Colors.black),
                     decoration: InputDecoration(
                       labelText: ("Date Of Birth:"),
@@ -122,8 +123,8 @@ class _RegisterState extends State<Register> {
                       },
                     ),
                   ),
-                  SizedBox(height: 15),
-                  TextField(
+                  const SizedBox(height: 15),
+                  const TextField(
                     style: TextStyle(color: Colors.black),
                     decoration: InputDecoration(
                       labelText: ("Enter password:"),
