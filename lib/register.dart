7a6dc65c242bf3a7a63bc23369e82159b459a3af
thresmarie <thresmariemathew08@gmail.com>
2022-12-01 05:14:04 -0800

    ....

--- lib/register.dart
+++ lib/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whaleshark/login.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -16,10 +17,10 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.lightBlue.shade200,
+       backgroundColor: Colors.lightBlue.shade200,
       appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
-        title: const Text("Registration"),
+        backgroundColor: Colors.white,
+        title: const Text("Registration",style: TextStyle(color: Colors.blue),),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -97,56 +98,88 @@ class _RegisterState extends State<Register> {
                   SizedBox(
                     height: 5,
                   ),
-                  Row(
+                  Column(
                     children: [
                       Text(
                         "Choose Gender:",
                         style: TextStyle(fontSize: 16),
                       ),
+                      Row(
+                        
+                        children: [
+                           SizedBox(height: 20,),
+                          Container(
+                        
+                        height: 10,
+                        child: Radio<user>(
+                          value: user.female,
+                          groupValue: _gender,
+                          onChanged: (user? value) {
+                            setState(() {
+                              _gender = value;
+                              print(value);
+                            });
+                          },
+                        ),
+                      ),Text("Female"),
+                            SizedBox(
+                            height: 20,
+                              ),
+                        
+                        ],
+                      ),
+                      Row(
+                      children:[
+                        
+                      SizedBox(height: 20),
                       Container(
                         height: 10,
-                        child:Radio<user>(
-                             value: user.female,
-                             groupValue: _gender,
-                             onChanged: (user? value){
-                              setState(() {
-                                _gender=value;
-                              });
-                             },
+                        child: Radio<user>(
+                          value: user.male,
+                          groupValue: _gender,
+                          onChanged: (user? value) {
+                            setState(() {
+                              _gender = value;
+                              print(value);
+                            });
+                          },
                         ),
                       ),
+                      Text("Male"),
+                            SizedBox(
+                            height: 20,
+                              ),
+                      ],
+                      ),
+                      Row(
+                        children:[
+                        SizedBox(height: 20),  
                       Container(
                         height: 10,
-                        child:Radio<user>(
-                             value: user.male,
-                             groupValue: _gender,
-                             onChanged: (user? value){
-                              setState(() {
-                                _gender=value;
-                              });
-                             },
+                        child: Radio<user>(
+                          value: user.other,
+                          groupValue: _gender,
+                          onChanged: (user? value) {
+                            setState(() {
+                              _gender = value;
+                              print(value);
+                            });
+                          },
                         ),
                       ),
-                      Container(
-                        height: 10,
-                        child:Radio<user>(
-                             value: user.other,
-                             groupValue: _gender,
-                             onChanged: (user? value){
-                              setState(() {
-                                _gender=value;
-                              });
-                             },
-                        ),
+                      Text("Others"),
+                            SizedBox(
+                            height: 20,
+                              ),],
                       ),
                     ],
-                    ),
-                       
-
+                  ),
+                  
+                  
                   //       child: RadioListTile<user>(
                   //         tileColor: Colors.white,
                   //         title: const Text("Female"),
-                          
+
                   //           value: user.female,
                   //           groupValue: _gender,
                   //           onChanged: (user? value) {
@@ -154,7 +187,7 @@ class _RegisterState extends State<Register> {
                   //               _gender = value;
                   //             });
                   //           },
-                          
+
                   //       ),
                   //     ),
                   //     Container(
@@ -162,7 +195,7 @@ class _RegisterState extends State<Register> {
                   //       child: RadioListTile<user>(
                   //         tileColor: Colors.white,
                   //         title: const Text("Male"),
-                          
+
                   //           value: user.male,
                   //           groupValue: _gender,
                   //           onChanged: (user? value) {
@@ -170,7 +203,7 @@ class _RegisterState extends State<Register> {
                   //               _gender = value;
                   //             });
                   //           },
-                          
+
                   //       ),
                   //     ),
                   //     Container(
@@ -178,7 +211,7 @@ class _RegisterState extends State<Register> {
                   //       child: RadioListTile<user>(
                   //         tileColor: Colors.white,
                   //         title: const Text("Others"),
-                          
+
                   //           value: user.other,
                   //           groupValue: _gender,
                   //           onChanged: (user? value) {
@@ -186,12 +219,12 @@ class _RegisterState extends State<Register> {
                   //               _gender = value;
                   //             });
                   //           },
-                          
+
                   //       ),
                   //     ),
                   //   ],
                   // ),
-                      
+
                   SizedBox(height: 15),
                   TextField(
                     style: TextStyle(color: Colors.black),
@@ -220,6 +253,23 @@ class _RegisterState extends State<Register> {
                       ),
                     ),
                   ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  MaterialButton(
+                      color: Colors.white,
+                      height: 40,
+                      minWidth: 90,
+                      textColor: Colors.black,
+                      child: Text("Sumbit"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const Login(),
+                          ),
+                        );
+                      })
                 ],
               ),
             ),
