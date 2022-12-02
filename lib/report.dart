615713630fca56fe2b3a1c05db90ae776dc820ed
nicerosebenny <117380574+nicerosebenny@users.noreply.github.com>
2022-12-02 17:49:40 +0530

    profile

--- lib/report.dart
+++ lib/report.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:whaleshark/login.dart';
 
 class Report extends StatefulWidget {
   const Report({Key? key}) : super(key: key);
@@ -20,70 +21,117 @@ class _ReportState extends State<Report> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Row(
-            children: [
-              SizedBox(
-                width: 30,
-              ),
-              Icon(Icons.arrow_back_ios),
-              Text(
-                "Report",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 50,
-                ),
-              ),
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 241, 243, 245),
+        title: Row(
+          children: [
+            Text(
+              "Report",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 41, 86, 211),
+                  fontSize: 38,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
-        backgroundColor: Color.fromARGB(255, 104, 177, 241),
-        body: Container(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Center(
-                      child: InkWell(
-                    child: Image.asset("assets/whale.png",width: 200,),
-                    onTap: () {
-                      Text(
-                        "In net",
-                        style: TextStyle(color: Colors.black),
-                      );
-                      setState(() {
-                        check == false
-                            ? check = true
-                            : check == true
-                                ? check = false
-                                : false;
-                      });
-                    },
-                  )),
-                  Center(
-                      child: InkWell(
-                    child: Image.asset("assets/clownfish-r.jpg",width: 200,height: 100,),
-                    onTap: () {
-                      setState(() {
-                        check == false
-                            ? check = true
-                            : check == true
-                                ? check = false
-                                : false;
-                      });
-                    },
-                  )),
-                ],
-              ),
-              check == false
-                  ? SizedBox.shrink()
-                  : Container(
-                      height: 40,
-                      color: Colors.red,
-                    )
-            ],
-          ),
-        ));
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Image.asset("assets/water2.jpg"),
+            SizedBox(
+              
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(22),
+                    child: Container(
+                      width: 150,
+                      color: Color.fromARGB(255, 77, 201, 220),
+                      child: Center(
+                          child: InkWell(
+                        child: Image.asset(
+                          "assets/whale.png",
+                          width: 200,
+                          height: 100,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            check == false
+                                ? check = true
+                                : check == true
+                                    ? check = false
+                                    : false;
+                          });
+                        },
+                      )),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(22),
+                    child: Container(
+                      width: 150,
+                      color: Color.fromARGB(255, 77, 201, 220),
+                      child: Center(
+                          child: InkWell(
+                        child: Image.asset(
+                          "assets/clownfish-r.png",
+                          width: 100,
+                          height: 100,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            check == false
+                                ? check = true
+                                : check == true
+                                    ? check = false
+                                    : false;
+                          });
+                        },
+                      )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            check == false
+                ? SizedBox.shrink()
+                : Column(
+                    children: [
+                      MaterialButton(
+                          child: Text(
+                            "In net",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.black,
+                          onPressed: () {}),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      MaterialButton(
+                        child: Text(
+                          "Natural water",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.black,
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Login()));
+                        },
+                      )
+                    ],
+                  ),
+          ]),
+        ),
+      ),
+    );
   }
 }
